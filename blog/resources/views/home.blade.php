6d7b5e483b5752e9c05026f6a86e14e35750b7bd
Alexandre Payen <alexandrepayen@yahoo.fr>
2016-10-10 16:08:59 +0200

    test

--- blog/resources/views/home.blade.php
+++ blog/resources/views/home.blade.php
@@ -8,9 +8,34 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="block-recette" style="height: 100px; background: gold">
+                                <strong>Titre</strong>
+                                <p>Info</p>
+                            </div>
+                        </div>
+
+
+
+                        <div class="col-md-3">
+                            <div class="block-add" style="height: 100px; background: gold">
+                                <i class="glyphicon glyphicon-plus"></i>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+        </div>
+    </div>
+</div>
+
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-sm">Small modal</button>
+
+<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+    <div class="modal-dialog modal-sm" role="document">
+        <div class="modal-content">
+            ...
         </div>
     </div>
 </div>
