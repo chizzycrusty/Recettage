59d4c149b4e759b4708833b563a2fd12718029da
William Wong <william.wong0613@gmail.com>
2016-10-10 15:12:01 +0200

    Commit test

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Marvin sent mauvais
                 </div>
 
                 <div class="links">
